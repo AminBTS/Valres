189763f5597d7cd22cf17333c5befb840a6050a5
GHAMZI AMIN <GHAMZIA@ozenne.info>
2023-11-16 10:53:03 +0100

    Index, login, logout, et d'autres fichiers ajouté

---
+++ includes/_inc_header.php
@@ -0,0 +1,19 @@
+<!-- Création du Header -->
+<header>
+    <!-- Import du Logo à gauche -->
+        <a href="/"><div class="logoheader" alt="Valres" title="Valres">V</div></a>
+        <nav>
+            <ul>
+                <!-- Import des onglets et leurs lien de navigation -->
+                <li><a href="reservation.php">Réservations</a></li>
+                <li><a href="/">Contact</a></li>
+            </ul>
+        </nav>
+            <a href="../users/login">Utilisateur non identifié</a>
+        </div>
+        
+    </div>
+</header>
+
+<!-- Import du CSS pour le Header -->
+<link rel="stylesheet" href="css/header.css">
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Valres - Accueil</title>
+</head>
+<body>
+    <!-- Header importé -->
+    <?php include_once("includes/_inc_header.php") ?>
+</body>
+</html>
