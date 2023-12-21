31e6494011f17d7eb61eec319782e19a7e904c01
GHAMZI AMIN <GHAMZIA@ozenne.info>
2023-12-21 08:48:45 +0100

    v21.12.2023

--- modele/data_process.php
+++ modele/data_process.php
@@ -1,6 +1,20 @@
 <?php
 // Inclusion du fichier de connexion à la base de données
 // Connexion à la base de données
+$serveur = "localhost";
+$utilisateur = "root";
+$mot_de_passe = "";
+$base_de_donnees = "valres"; 
+
+try {
+    $connexion = new PDO("mysql:host=$serveur;dbname=$base_de_donnees;charset=utf8", $utilisateur, $mot_de_passe);
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Affiche l'erreur
+    die("Erreur dans la connexion à la base de donnée: " . $e->getMessage());
+    // Redirige vers index.php
+    header("Location: /");
+}
 
 // Vérification des données du formulaire
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email']) && isset($_POST['motdepasse'])) {
